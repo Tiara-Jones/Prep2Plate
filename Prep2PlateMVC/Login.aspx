0c1975ea5fc9bc6d5abcb9e806785242bcea7f69
shanthikannan.d <shanthikannan.d@gmail.com>
2017-11-13 23:41:58 -0500

    Adding Login Webforms

---
+++ Prep2PlateMVC/Login.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Prep2PlateMVC.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+    
+    <a href="/Registration.aspx">Click Here to Register</a>
+</body>
+</html>
---
+++ Prep2PlateMVC/Login.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prep2PlateMVC
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //protected void Button1_Click(object sender, EventArgs e)
+        //{
+
+        //    //TODO: Parameterized Query
+        //    //TODO: Validate and return failure, if email already 
+        //    //TODO: Add back User Name if needed UserName, '{TextBoxUN.Text}',
+        //    try
+        //    {
+        //        string connectionString = System.Configuration.ConfigurationManager
+        //            .ConnectionStrings["Prep2PlateConnectionString"].ConnectionString;
+        //        using (SqlConnection connection = new SqlConnection(connectionString))
+        //        {
+        //            connection.Open();
+
+        //            string checkuser = "select count(*) from Users where Email ='" + TextBoxEmail.Text + "' AND Password='" + TextBoxPW.Text + "'";
+        //            SqlCommand com = new SqlCommand(checkuser, connection);
+        //            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+        //            if (temp == 0)
+        //            {
+        //                Response.Write("Login NOT successful. Please Retry..!");
+        //                return;
+        //            }
+
+        //            Response.Redirect("~/");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Response.Write("Error:" + ex.ToString());
+        //    }
+
+        //}
+
+    }
+}
